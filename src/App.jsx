2675a2123ab83f97cd798ff48ea0ec7c137e4266
Bruno Francisco <64901068+brunofranciscu@users.noreply.github.com>
2024-07-22 16:58:05 -0300

    better favorite icons

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,11 @@ export default function App() {
   return (
     <div className="App">
       <Nav/>
-      <div className='presentation duration-200 transition-all bg-auto bg-[#e5e5e5] flex relative overflow-hidden z-0'>
+      <div className='presentation duration-200 transition-all bg-[#e5e5e5] relative overflow-hidden z-0 my-[70px] max-w-[1100px] mx-auto sm:py-7 sm:px-16 text-left rounded-3xl flex bg-[length:100%,50%] px-8 py-8'>
 
         <div className="z-10 relative">
-          <span>BRUNO FRANCISCO</span>
-          <h1 className="font-bold text-gray-200 leading-none">Estudos de ilustras e rabiscos.</h1>
+          <span className="text-gray-700 dark:text-gray-200">BRUNO FRANCISCO</span>
+          <h1 className="font-bold text-gray-700 dark:text-gray-100 leading-none m-0 w-full text-[clamp(1rem,_0.284rem_+_3.9506vw,_3rem)]">Estudos de ilustras e rabiscos.</h1>
         </div>
 
         <div style={{ backgroundImage: `linear-gradient(to right,currentColor, transparent), url(${bg})` }} 
--- src/mansonry.jsx
+++ src/mansonry.jsx
@@ -10,9 +10,10 @@ import '@splidejs/react-splide/css';
 
 const Mansonry = () => {
     const deletar = `<svg fill="#000000" width="15px" height="15px" viewBox="0 0 408.483 408.483"> <g> <g> <path d="M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316 H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293 c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329 c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355 c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356 c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z"/> <path d="M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305h-92.971c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916 c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z"/> </g> </g> </svg>`
-    const star = `<svg width="20px" height="20px" viewBox="0 0 24 24" ><path d="M12 2.5a1 1 0 0 1 .894.553l2.58 5.158 5.67.824a1 1 0 0 1 .554 1.706l-4.127 4.024.928 5.674a1 1 0 0 1-1.455 1.044L12 18.807l-5.044 2.676a1 1 0 0 1-1.455-1.044l.928-5.674-4.127-4.024a1 1 0 0 1 .554-1.706l5.67-.824 2.58-5.158A1 1 0 0 1 12 2.5zm0 3.236l-1.918 3.836a1 1 0 0 1-.75.543l-4.184.608 3.05 2.973a1 1 0 0 1 .289.878L7.8 18.771l3.731-1.98a1 1 0 0 1 .938 0l3.731 1.98-.687-4.197a1 1 0 0 1 .289-.877l3.05-2.974-4.183-.608a1 1 0 0 1-.75-.543L12 5.736z" fill="#0D0D0D"/></svg>`
-    const starred = `<svg fill="#000000" width="20px" height="20px" viewBox="0 0 24 24" enable-background="new 0 0 24 24"><path d="M22,10.1c0.1-0.5-0.3-1.1-0.8-1.1l-5.7-0.8L12.9,3c-0.1-0.2-0.2-0.3-0.4-0.4C12,2.3,11.4,2.5,11.1,3L8.6,8.2L2.9,9C2.6,9,2.4,9.1,2.3,9.3c-0.4,0.4-0.4,1,0,1.4l4.1,4l-1,5.7c0,0.2,0,0.4,0.1,0.6c0.3,0.5,0.9,0.7,1.4,0.4l5.1-2.7l5.1,2.7c0.1,0.1,0.3,0.1,0.5,0.1l0,0c0.1,0,0.1,0,0.2,0c0.5-0.1,0.9-0.6,0.8-1.2l-1-5.7l4.1-4C21.9,10.5,22,10.3,22,10.1z"/></svg>`
-    
+    const saveIcon = `<svg width="20px" height="20px" viewBox="0 0 64 64"><path d="M30.051 45.6071L17.851 54.7401C17.2728 55.1729 16.5856 55.4363 15.8662 55.5008C15.1468 55.5652 14.4237 55.4282 13.7778 55.1049C13.1319 54.7817 12.5887 54.2851 12.209 53.6707C11.8293 53.0563 11.6281 52.3483 11.628 51.626V15.306C11.628 13.2423 12.4477 11.2631 13.9069 9.8037C15.3661 8.34432 17.3452 7.52431 19.409 7.52405H45.35C47.4137 7.52431 49.3929 8.34432 50.8521 9.8037C52.3112 11.2631 53.131 13.2423 53.131 15.306V51.625C53.1309 52.3473 52.9297 53.0553 52.55 53.6697C52.1703 54.2841 51.6271 54.7807 50.9812 55.1039C50.3353 55.4272 49.6122 55.5642 48.8928 55.4998C48.1734 55.4353 47.4862 55.1719 46.908 54.739L34.715 45.6071C34.0419 45.1031 33.2238 44.8308 32.383 44.8308C31.5422 44.8308 30.724 45.1031 30.051 45.6071V45.6071Z" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/></svg>`
+    const savedIcon = `<svg width="20px" height="20px" viewBox="0 0 64 64"><path d="M45.35 6.1709H19.41C16.8178 6.17618 14.3333 7.20827 12.5003 9.04123C10.6674 10.8742 9.63528 13.3587 9.62999 15.9509V52.2709C9.6272 53.3655 9.92973 54.4392 10.5036 55.3713C11.0775 56.3034 11.9 57.057 12.8787 57.5474C13.8573 58.0377 14.9533 58.2454 16.0435 58.1471C17.1337 58.0488 18.1748 57.6484 19.05 56.9909L31.25 47.8509C31.5783 47.6074 31.9762 47.4759 32.385 47.4759C32.7938 47.4759 33.1917 47.6074 33.52 47.8509L45.71 56.9809C46.5842 57.6387 47.6246 58.0397 48.7142 58.1387C49.8038 58.2378 50.8994 58.0311 51.8779 57.5418C52.8565 57.0525 53.6793 56.3001 54.2537 55.3689C54.8282 54.4378 55.1317 53.365 55.13 52.2709V15.9509C55.1247 13.3587 54.0926 10.8742 52.2597 9.04123C50.4267 7.20827 47.9422 6.17618 45.35 6.1709Z" stroke-width="4"/></svg>`
+    const loadSpin = `<svg width="20px" height="20px" viewBox="0 0 16 16" fill="none" class="hds-flight-icon--animation-loading"><g fill="#000000" fill-rule="evenodd" clip-rule="evenodd"><path d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z" opacity=".2"/><path d="M7.25.75A.75.75 0 018 0a8 8 0 018 8 .75.75 0 01-1.5 0A6.5 6.5 0 008 1.5a.75.75 0 01-.75-.75z"/></g></svg>`
+
     const { categoria } = useCategoria();
     const { logado } = useAuth();
     const [files, setFiles] = useState([]);
@@ -25,6 +26,8 @@ const Mansonry = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0); 
     const [favorite, setFavorite] = useState({});
     const [viewFav, setViewFave] = useState(false)
+    const [modalColor, setModalColor] = useState('')
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchUrls = async () => {
@@ -93,6 +96,7 @@ const Mansonry = () => {
         setCurrentCategoryImages(categoria);
         const clickedImageIndex = categoria.findIndex(image => image.url === url);
         setCurrentImageIndex(clickedImageIndex);
+        setModalColor(colors[url])
     };
     
     const dataUpload = (hour) => new Intl.DateTimeFormat('pt','BR').format(new Date(hour))
@@ -113,27 +117,28 @@ const Mansonry = () => {
 
     return (
         <>
-            <button className='fixed md:right-12 right-[unset] left-5 md:left-[unset] top-[17px] z-50 [&>svg_path]:fill-gray-300 hover:brightness-150 duration-100 ' dangerouslySetInnerHTML={{__html:starred }} onClick={() => setViewFave(true)}></button>
+            <button className='fixed md:right-12 right-[unset] left-5 md:left-[unset] top-[17px] z-50 [&>svg_path]:fill-gray-500 dark:[&>svg_path]:fill-gray-300 hover:brightness-150 duration-100' 
+                    dangerouslySetInnerHTML={{__html:saveIcon }} onClick={() => setViewFave(true)}></button>
     
             {viewFav && 
-                <div className="fixed right-0 top-0 h-full md:w-[400px] w-full dark:bg-[#000000cc] bg-[#ddddddcc] backdrop-blur-sm z-50 overflow-y-scroll p-8">
+                <div className="fixed right-0 top-0 h-full md:w-[400px] w-full dark:bg-[#3a3b3cee] shadow-2xl bg-[#ddddddcc] backdrop-blur-sm z-50 overflow-y-scroll p-8">
                     <div className="flex gap-3">
                         <button onClick={() => setViewFave(false)} className='w-5 h-5 rounded-full p-2 leading-[0] z-50 cursor-pointer'>&lt;</button>
-                        <h3 className="text-gray-700 dark:text-gray-300 font-bold text-left">Ilustras Favoritadas</h3>
+                        <h3 className="text-gray-700 dark:text-gray-300 font-bold text-left">Favoritadas</h3>
                     </div>
                     <div className="flex flex-wrap gap-5 justify-stretch mx-auto py-8">
                         {Object.entries(favorite).map((item, index) => (
     
                             <>
                                 {item[1] ? 
-                                    <figure key={index} className="relative">
-                                        <button className="bg-red-500 text-gray-100 w-8 h-8 rounded-full absolute -top-3 -right-1 text-base [scale:.6] leading-[0]" onClick={()=>favoritar(item[0])}>x</button>
-                                        <img src={item[0]}  className="w-28 h-28 object-contain rounded-md"/>
+                                    <figure key={index} className="relative  [&:has(:hover)_img]:brightness-75 duration-100 [&:has(:hover)_svg]:fill-gray-100">
+                                        <button dangerouslySetInnerHTML={{__html:deletar}} onClick={()=>favoritar(item[0])}
+                                                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 [scale:.8] z-10"></button>
+                                        <img src={item[0]}  className="w-28 h-28 object-cover object-center rounded-md z-0"/>
                                     </figure> : ''}
                             </>
-    
                         ))
-                        }
+                    }
                     </div>
                 </div>
             }
@@ -152,29 +157,33 @@ const Mansonry = () => {
                                     </button>
                                 )}
     
-                                <img
-                                    className='bg z-40'
-                                    loading="lazy"
-                                    src={url}
-                                    onClick={() => abrirModal(url, pastinha.img)}
-                                    alt={`${pastinha.cat} | BRUNO FRANCISCO`}  
+                                <img className='bg z-40'
+                                     loading="lazy"
+                                     src={url}
+                                     onClick={() => abrirModal(url, pastinha.img)}
+                                     alt={`${pastinha.cat} | BRUNO FRANCISCO`}
                                 />
                                 <figcaption className='flex flex-col justify-end text-left ' >
                                     <span className='text-base text-gray-200 font-semibold leading-none'>{pastinha.cat}</span>
                                     <time className='text-[.6rem] text-gray-300 leading-none'>enviado: {dataUpload(timeCreated)}</time>
                                 </figcaption>
     
-                                <button dangerouslySetInnerHTML={{__html: favorite[url] ? starred : star}}
-                                        className='[&>svg_path]:fill-yellow-400 hover:brightness-150 duration-100 z-50 absolute bottom-2 right-2'
+                                {favorite[url] && <button dangerouslySetInnerHTML={{__html:saveIcon}}
+                                        className='[&>svg_path]:fill-gray-500 hover:brightness-150 duration-100 z-50 absolute top-2 right-2'
                                         onClick={() => favoritar(url)}>
-                                </button>
-    
+                                </button>}
+
+                                {!favorite[url] && <button dangerouslySetInnerHTML={{__html:savedIcon}}
+                                        className='[&>svg_path]:stroke-gray-500 [&>svg_path]:fill-none hover:brightness-150 duration-100 z-50 absolute top-2 right-2'
+                                        onClick={() => favoritar(url)}>
+
+                                </button>}
                             </figure>
                         ))}
                     </span>
                 ))}
                 {confirmation && 
-                    <div className='fixed bg-[#00000066] w-full h-[100dvh] top-0 left-0 grid place-items-center z-[999999]'>
+                    <div className='fixed bg-[#00000066] w-full h-[100dvh] top-0 left-0 z-[999999]'>
                         <div className='flex flex-col justify-center items-center bg-gray-200/30 backdrop-blur-md rounded-xl px-10 py-5 gap-4 max-w-[300px] w-[90%] border-gray-400/60 border-2 shadow-2xl'>
                             <h2 className='font-semibold text-gray-50'>TEM CERTEZA?</h2>
                             <div className='flex justify-center items-center gap-4'>
@@ -185,13 +194,13 @@ const Mansonry = () => {
                     </div>
                 }
                 {modal && (
-                    <div className="modal bg-[#00000033]">
-                        <a onClick={() => setModal(false)} className='bg-black w-8 h-8 rounded-full p-2 leading-none z-50 cursor-pointer'>X</a>
-    
-                        <Splide options={{type:'loop', rewind:true, start:currentImageIndex, pagination:false}}>
+                    <div className="fixed z-[60] w-full h-dvh backdrop-blur-md backdrop-saturate-[.1] backdrop-brightness-[.3] saturate-[1.3] left-0 top-0 grid place-items-center">
+                        <a onClick={() =>setModal(false)} 
+                            className={`bg-black w-8 h-8 rounded-full p-2 leading-none cursor-pointer absolute z-50 text-gray-300 hover:text-white md:top-10 md:right-12 top-[unset] right-[unset] left-1/2 bottom-8 md:left-[unset] md:bottom-[unset]`}>X</a>
+                        <Splide options={{type:'loop', rewind:true, start:currentImageIndex, pagination:false, keyboard:true, wheel:true}}>
                             {currentCategoryImages.map(({ url }, index) => (
-                                <SplideSlide key={index}>
-                                    <img src={url} alt={`Image ${index}`} className='w-auto h-[95dvh] block mx-auto rounded-3xl object-contain'/>
+                                <SplideSlide key={index}>                                  
+                                   <img src={url} alt={`Image ${index}`} className='w-auto h-[95dvh] block mx-auto rounded-3xl object-contain'/>
                                 </SplideSlide>
                             ))}
                         </Splide>
