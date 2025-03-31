e59a88152e94e544333f009c387f969ae2a5c4ed
Bruno Francisco <64901068+brunofranciscojs@users.noreply.github.com>
2025-03-31 00:07:31 -0300

    Update supabaseClient.jsx

--- src/contexto/supabaseClient.jsx
+++ src/contexto/supabaseClient.jsx
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://utyaegtlratnhymumqjm.supabase.co';
-const supabaseKey = import.meta.env.VITE_API_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0eWFlZ3RscmF0bmh5bXVtcWptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzNTU5MDUsImV4cCI6MjA1ODkzMTkwNX0.B2-GTy9rJPgGTmDeB70CwfzbbTTdocp-1QzRaMNDntQ'
 export const supabase = createClient(supabaseUrl, supabaseKey);
