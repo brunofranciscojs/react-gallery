ca98c816a4369c8018ff8fb1f23ee4298a818b38
Bruno Francisco <ednamaral39@yahoo.com.br>
2026-04-21 08:25:14 -0300

    Atualizar o layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,35 +4,46 @@ import '../styles/Nav.scss';
 import ClientLayout from '@/components/ClientLayout';
 
 export const metadata = {
-    title: 'Ilustras Bruno',
+    metadataBase: new URL('https://ilustracoes.brunofrancisco.com.br'),
+    title: 'Ilustras Bruno Francisco',
     description: 'Galeria de ilustrações desenvolvida com react, supabase e tailwindcss. Ilustrações feitas por mim.',
     keywords: ['ilustrações', 'galeria', 'react', 'supabase', 'tailwindcss'],
+
+    alternates: {
+        canonical: '/',
+    },
+
     openGraph: {
         title: 'Ilustras Bruno',
         description: 'Galeria de ilustrações desenvolvida com react, supabase e tailwindcss. Ilustrações feitas por mim.',
         type: 'website',
         locale: 'pt-BR',
+        url: 'https://ilustracoes.brunofrancisco.com.br',
     },
+
     twitter: {
         title: 'Ilustras Bruno',
         description: 'Galeria de ilustrações desenvolvida com react, supabase e tailwindcss. Ilustrações feitas por mim.',
         card: 'summary_large_image',
     },
+
     themeColor: '#000000',
     colorScheme: 'dark',
     viewport: {
         width: 'device-width',
         initialScale: 1,
     },
+verification: {
+    google: 'p2mjsXSTpbXUcYyXfMoDrnY5NOlyKEXK8-qFYL8NzYk',
+    other: {
+        'msvalidate.01': '5C82E1CA55822BA2966B04EA608CCA0D',
+    },
+},
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="pt-BR">
-            <head>
-                <meta name="google-site-verification" content="p2mjsXSTpbXUcYyXfMoDrnY5NOlyKEXK8-qFYL8NzYk" />
-                <meta name="msvalidate.01" content="5C82E1CA55822BA2966B04EA608CCA0D" />
-            </head>
             <body>
                 <ClientLayout>
                     {children}
